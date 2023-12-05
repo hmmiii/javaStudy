a2881bf947a2cb2257bceab4e469b459e7dd145b
hmmi <hmmi@kakao.com>
2023-12-05 18:25:19 +0900

    기본형 타입 형변환

---
+++ src/typecast.java
@@ -0,0 +1,9 @@
+public class typecast {
+    public static void main(String[] args) {
+        int intA = (int)'A';
+        char charA = (char)intA;
+        System.out.println(intA); // 65
+        System.out.println(charA); // 'A'
+        System.out.println(intA); // 65
+    }
+}
