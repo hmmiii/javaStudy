2d48eb1ed8ffb12c36795387fd4bf78b374ba3ab
hmmi <hmmi@kakao.com>
2023-12-07 17:22:17 +0900

    배열 생성과 조회

---
+++ src/ArrayCreate.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ArrayCreate {
+    public static void main(String[] args) {
+        int[] intArr = {10, 20, 30, 40, 50};
+
+        System.out.println(Arrays.toString(intArr)); // [10, 20, 30, 40, 50]
+
+        char[] chArr = {'a', 'b', 'c', 'd'};
+
+        System.out.println(chArr); // abcd
+    }
+}
