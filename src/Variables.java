c8ab62f2b2991e08c0254e43bb26bd69084ed9f2
hmmi <hmmi@kakao.com>
2023-12-12 22:43:21 +0900

    클래스 변수, 인스턴스 변수, 지역 변수

---
+++ src/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    // 클래스 영역
+    int iv; // 인스턴스 변수
+    static int cv; // 클래스 변수(static 변수, 공유변수)
+
+    void method(){
+        // 메서드 영역
+        int lv = 0; // 지역변수
+    }
+}
