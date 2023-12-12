4235d9c8b3309100979084c77e5caf255bb8793a
hmmi <hmmi@kakao.com>
2023-12-13 01:36:56 +0900

    콜스택

---
+++ src/CallStackTest.java
@@ -0,0 +1,26 @@
+public class CallStackTest {
+    public static void main(String[] args) {
+        firstMethod();
+    }
+
+    static void firstMethod(){
+        System.out.println("firstMethod() 시작");
+        secondMethod();
+        System.out.println("firstMethod() 종료");
+    }
+
+    static void secondMethod(){
+        System.out.println("secondMethod() 시작");
+        System.out.println("secondMethod() 종료");
+    }
+
+}
+
+/*
+출력
+
+firstMethod() 시작
+secondMethod() 시작
+secondMethod() 종료
+firstMethod() 종료
+*/
