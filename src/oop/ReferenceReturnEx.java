15fbba50556f34e67d8bcb283d04bf31b87885df
hmmi <hmmi@kakao.com>
2023-12-13 18:24:56 +0900

    참조형 반환타입

--- src/oop/ReferenceReturnEx.java
+++ src/oop/ReferenceReturnEx.java
@@ -2,6 +2,18 @@ package oop;
 
 public class ReferenceReturnEx {
     public static void main(String[] args) {
+        Data d = new Data();
+        d.x = 10;
 
+        Data d2 = copy(d);
+        System.out.println("d.x = "+d.x);
+        System.out.println("d.x = "+d2.x);
+    }
+
+    static Data copy(Data d){
+        Data tmp = new Data(); // 새로운 객체 tmp를 생성한다.
+        tmp.x = d.x; // d.x의 값을 tmp.x에 복사한다.
+
+        return tmp; // 복사한 객체의 주소를 반환한다.
     }
 }
