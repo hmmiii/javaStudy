69fef31f9c1ef223065f1ba8c5b7fee94c4e3376
hmmi <hmmi@kakao.com>
2023-12-22 19:04:18 +0900

    생성자에서 다른 생성자 호출 - this, this()

---
+++ src/oop/CarTest2.java
@@ -0,0 +1,32 @@
+package oop;
+
+class Car2{
+    String color; // 색상
+    String gearType; // 변속기 종류 - auto(자동), manual(수동)
+    int door; // 문 개수
+
+    Car2(){
+        this("white", "auto", 4); // Car2(String c, String g, int d) 호출
+    }
+
+    Car2(String color){
+        this(color, "auto", 4); // Car2(String c, String g, int d) 호출
+    }
+
+    Car2(String c, String g, int d){ // 생성자
+        color = c;
+        gearType = g;
+        door = d;
+    }
+}
+
+public class CarTest2 {
+    public static void main(String[] args) {
+        Car2 c1 = new Car2();
+
+        Car2 c2 = new Car2("blue");
+
+        System.out.println("c1의 color = " + c1.color + ", gearType = " + c1.gearType + ", door = " + c1.door);
+        System.out.println("c2의 color = " + c2.color + ", gearType = " + c2.gearType + ", door = " + c2.door);
+    }
+}
