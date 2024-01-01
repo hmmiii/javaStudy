f979f154aad690eadfdee821b3afb937d29ae6ff
hmmi <hmmi@kakao.com>
2024-01-01 20:15:59 +0900

    study : Vector 클래스 사용

--- src/oop/ProductTest.java
+++ src/oop/ProductTest.java
@@ -1,4 +1,7 @@
 package oop;
+import java.util.*;
+
+import java.util.Vector;
 
 public class ProductTest {
     public static void main(String[] args) {
@@ -11,6 +14,8 @@ public class ProductTest {
 
         b.buy(x);
         b.buy(a);
+
+        b.summary();
     }
 }
 
@@ -55,7 +60,7 @@ class Audio extends Product{
 class Buyer{
     int money = 1000;
     int bonusPoint = 0;
-    Product[] item = new Product[10]; // 구입한 제품을 저장하기 위한 배열
+    Vector item = new Vector(); // 구입한 제품을 저장하기 위한 배열
     int i = 0;
 
 
@@ -70,7 +75,37 @@ class Buyer{
 
         // Buyer의 보너스점수에 제품의 보너스점수를 더한다.
         bonusPoint += p.bonusPoint;
-        item[i++] = p; // 제품을 Product[] item에 저장
+        item.add(p);
         System.out.println(p + " 을 구매하셨습니다.");
+    }
+
+    void refund(Product p){ // 구입한 제품을 환불한다.
+        if (item.remove(p)){ // 제품을 Vector에서 제거한다.
+            money += p.price;
+            bonusPoint -= p.bonusPoint;
+            System.out.println(p + " 을 반품하셨습니다.");
+        }else{ // 제거에 실패한 경우
+            System.out.println("구입한 제품 중 해당 제품이 없습니다.");
+        }
+    }
+
+    void summary(){ // 구입한 물품에 대한 정보를 요약해서 보여준다.
+        int sum = 0; // 구입한 물품의 합계
+        String itemList = ""; // 구입한 물품목록
+
+        if (item.isEmpty()){ // Vector 비어있는지 확인한다.
+            System.out.println("구입한 제품이 없습니다.");
+            return;
+        }
+
+        // 반복문을 이용해서 구입한 물품의 총 가격과 목록을 만든다.
+        for (int i = 0; i < item.size() ; i++) {
+            Product p = (Product) item.get(i); // Vector 객체의 i번째에 있는 객체를 얻어온다.
+            sum += p.price;
+            itemList += (i==0) ? "" + p : " , " + p;
+        }
+        System.out.println("구입하신 물품의 총금액은 " + sum + "만원입니다.");
+        System.out.println("구입하신 물품은 " + itemList + "입니다.");
+
     }
 }
