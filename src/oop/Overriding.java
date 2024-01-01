5c9e04b5f6495a92ff07cef6475cc270a4b89485
hmmi <hmmi@kakao.com>
2024-01-01 16:22:07 +0900

    study : 오버라이딩

--- src/oop/Overriding.java
+++ src/oop/Overriding.java
@@ -4,6 +4,11 @@ class Point{
     int x;
     int y;
 
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
     String getLocation(){
         return "x : " + x + ", y : " + y;
     }
@@ -12,13 +17,19 @@ class Point{
 class Point3D extends Point{
     int z;
 
+    Point3D(int x, int y, int z){
+        super(x, y);
+        this.z = z;
+    }
+
     String getLocation(){
-        return "x : " + x + ", y : " + y + ", z : " + z;
+//        return "x : " + x + ", y : " + y + ", z : " + z;
+        return super.getLocation() + ", z : " + z; // 조상 메서드 호출
     }
 }
 
 public class Overriding {
     public static void main(String[] args) {
-
+        Point3D p3 = new Point3D(10, 10, 10);
     }
 }
